45aa7bb97752e3c5a0c0da22169050e637f4dc71
hari-huynh <haihuynh.mathcs@gmail.com>
2024-11-08 05:05:51 +0700

    Update file train.py

--- train.py
+++ train.py
@@ -2,9 +2,6 @@ from transformers import TrainingArguments, Trainer
 from transformers import WavLMConfig
 from data.visedia_dataset import my_collate_fn, ViSEDia
 from model.model import SED_Model
-
-from data.visedia_dataset import SED_Model, my_collate_fn, ViSEDia
-
 from torchaudio.transforms import Resample
 import yaml
 import argparse
