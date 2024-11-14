03db2f244995f60bc96da8fd34aed8d130b0c3b0
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-11-14 16:44:44 +0700

    [update]

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 import os
 import librosa
 from scipy.io.wavfile import write
-from defaults import RAW_AUDIO_PATH, SAVE_PATH
+# from defaults import RAW_AUDIO_PATH, SAVE_PATH
+
+import yaml
+
+
+# Load configs
+with open("./ViSEDia/configs.yaml", "r") as file:
+    conf = yaml.safe_load(file)
+
 
 def save_audio(waveform, sample_rate, audio_name):
-    write(f"{SAVE_PATH}/{audio_name}.wav", sample_rate, waveform)
+    write(f"{conf['save_path']}/{audio_name}.wav", sample_rate, waveform)
 
 def load_audio(audio_id):
-    waveform, sample_rate = librosa.load(os.path.join(RAW_AUDIO_PATH, f"{audio_id}.mp3"))
+    waveform, sample_rate = librosa.load(os.path.join(conf['raw_audio_path'], f"{audio_id}.mp3"))
 
     return waveform, sample_rate
 
